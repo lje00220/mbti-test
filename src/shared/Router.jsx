6c36d6c371e7f538b7820589b4f848c534e95dd6
lje00220 <155710708+lje00220@users.noreply.github.com>
2025-02-20 14:21:45 +0900

    feat: 로그인, 회원가입, 홈 페이지 라우터 설정

---
+++ src/shared/Router.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "../pages/home/Home";
+import { Login } from "../pages/login/Login";
+import SignUp from "../pages/signup/SignUp";
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
