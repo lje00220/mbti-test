c8de8a9e5377315792ad86c3988504bfc43a4246
lje00220 <155710708+lje00220@users.noreply.github.com>
2025-02-24 21:33:20 +0900

    chore: 주석 추가, 공유하기 주소 재수정

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,6 +1,11 @@
 import axios from "axios";
 import useUserStore from "../zustand/userStore";
 
+/**
+ * 로그인, 회원가입, 회원 정보 수정과 같은
+ * 인증, 인가와 관련한 axios 함수
+ */
+
 const instance = axios.create({
   baseURL: import.meta.env.VITE_NBC_SERVER,
 });
@@ -13,16 +18,19 @@ instance.interceptors.request.use((config) => {
   return config;
 });
 
+// 회원가입
 export const register = async (userData) => {
   const response = await instance.post("/register", userData);
   return response.data;
 };
 
+// 로그인
 export const login = async (userData) => {
   const response = await instance.post("/login", userData);
   return response.data;
 };
 
+// 닉네임 수정
 export const updateProfile = async (formData) => {
   const response = await instance.patch("/profile", formData, {
     headers: {
